9463b282cb38d39092342fb5428cc1edb46a1dcf
Shahadat934 <Hossain79934@hmail.com>
2024-01-18 14:17:38 +0600

    first commit

---
+++ javascript/script.js
@@ -0,0 +1,5 @@
+function circleMouseFollower() {
+    window.addEventListener("mousemove", function (dets) {
+        document.querySelector(".minicircle").style.transform = 'translate(${dets.clientX}px, ${dets.clientY}px})';
+    })
+}
